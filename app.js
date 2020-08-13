dc8f9cbf8c3f12e0ea950f3848dfc1409ae80124
zerokira123 <thegetbackerzero@gmail.com>
2020-08-13 21:10:29 +0530

    readline module
    using read line module to  print  output according to correct and  incorrect answer

---
+++ app.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+const rl = readline.createInterface({input : process.stdin, output : process.stdout});
+
+let num1 = Math.floor((Math.random() *10) + 1);
+let num2 = Math.floor((Math.random() *10) + 1);
+let answer = num1 + num2;
+
+rl.question(`what is ${ num1 } + ${ num2 }? \n`,(userInput) => {
+
+    if(userInput.trim() == answer){
+        rl.close();
+    }
+    else{
+        rl.setPrompt('Incorrect Answer try again\n');
+        rl.prompt();
+
+        rl.on('line',(userInput) =>{
+            if(userInput.trim() == answer){
+                rl.close();
+            }
+            else{
+                rl.setPrompt(`your anser of ${ userInput } is Incorrect \n`);
+                rl.prompt();
+            }
+        })
+    
+    }
+
+})
+
+rl.on('close',() =>{
+    console.log('correct answer')
+})
