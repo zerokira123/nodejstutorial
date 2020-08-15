02ca6c66cc4a5482aa7ed66a0c50d753dbb5be1c
zerokira123 <thegetbackerzero@gmail.com>
2020-08-15 15:42:34 +0530

    read stream
    using read stream and write stream to read data from one file and writing the same data to another file using chunk at same time

---
+++ read stream/app.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const readStream = fs.createReadStream('./example.txt','utf8');
+const writeStream = fs.createWriteStream('example2.txt');
+
+
+
+readStream.on('data',(chunk) =>{
+    writeStream.write(chunk);
+});
+// console.log(readstrean);
