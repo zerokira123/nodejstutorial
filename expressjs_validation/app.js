5db0e82feeff8e28319c28714ec32ef5cabe26aa
zerokira123 <thegetbackerzero@gmail.com>
2020-08-17 00:07:40 +0530

    joi version not supported
    joi version 15 is required to validate the schema but its not available and i cant get response from post

---
+++ expressjs_validation/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyparser = require('body-parser');
+const Joi = require('joi');
+ 
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.use(bodyparser.urlencoded({extended:  false}));
+app.use(bodyparser.json());
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'static','index.html' ));
+});
+
+app.post('/',(req,res) =>{
+    console.log(req.body);
+    
+    const schema = Joi.object().keys({
+        email : Joi.string().trim().email().required(),
+        password : Joi.string().min(5).max(12).trim().required(),
+    });
+    Joi.validate(req.body,(err,result) => {
+        if(err){
+            console.log(err);
+            res.send('an error has occured');
+        }
+        else{
+            console.log(result);
+            res.send('succesfully posted data');
+
+        }
+
+    })
+});
+
+app.listen(3000);
