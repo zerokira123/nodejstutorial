166cf848a21587047105e7cfca9bd62cca2968d8
zerokira123 <thegetbackerzero@gmail.com>
2020-08-16 21:48:58 +0530

    express json
    using express to store data in json

---
+++ express_js_json/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyparser = require('body-parser');
+ 
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.use(bodyparser.urlencoded({extended:  false}));
+app.use(bodyparser.json());
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'static','index.html' ));
+});
+
+app.post('/',(req,res) =>{
+    console.log(req.body);
+    // database work here
+    res.json({success : true});
+});
+
+app.listen(3000);
