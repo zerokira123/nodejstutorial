036024cd71f5b18a974132cfefebfc6efaea0dcc
zerokira123 <thegetbackerzero@gmail.com>
2020-08-16 17:31:31 +0530

    Update app.js

--- express_static_files/app.js
+++ express_static_files/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use('/public',express.static(path.join(__dirname,'static')));
 
 app.get('/',(req,res) => {
-    res.send(path.join(__dirname,'static','index.html' ));
+    res.sendFile(path.join(__dirname,'static','index.html' ));
 });
 
 app.listen(3000);
